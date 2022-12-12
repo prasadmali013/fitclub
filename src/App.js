f357269fd9d467c79504ed9eea06730894535a2d
prasad mali <maliprasad26@gmail.com>
2022-12-12 14:40:00 +0530

    first-app

--- src/App.js
+++ src/App.js
@@ -1,8 +1,22 @@
 import './App.css';
+import Footer from './components/Hero/Footer/Footer';
+import Hero from './components/Hero/Hero';
+import Join from './components/Hero/Join/Join';
+import Plan from './components/Hero/Plans/Plan';
+import Programs from './components/Hero/Programs/Programs';
+import Reasons from './components/Hero/Reasons/Reasons';
+import Testimonial from './components/Hero/Testimonials/Testimonial';
+
 function App() {
   return (
     <div className="App">
-          <span style={{color: 'white'}}>Subscribe Zainkeepscode</span>
+      <Hero />
+      <Programs/>
+      <Reasons/>
+      <Plan/>
+      <Testimonial/>
+      <Join/>
+      <Footer/>
     </div>
   );
 }
---
+++ src/components/Hero/Programs/Programs.jsx
@@ -0,0 +1,39 @@
+import React from 'react'
+import './Programs.css'
+import {programsData} from './../../../data/programsData'
+import RightArrow from './../../../assets/rightArrow.png'
+
+const Programs = () => {
+    return (
+        <div className="programs" id="programs">
+
+            {/* Header */}
+            <div className="programs-header">
+                <span className='stroke-text'>Explore our </span>
+                <span>programs </span>
+                <span className='stroke-text'>to shape you </span>
+            </div>
+
+            <div className="program-categories">
+                {programsData.map((program) => ( 
+                    <div className="category">
+                        {program.image}
+                        <span>{program.heading}</span>
+                        <span>{program.details}</span>
+
+                        <div className="join-now"><span>join now</span> 
+                        <img src={RightArrow} alt="" />
+                        </div>
+                    </div>
+
+                ))}
+            </div>
+        
+
+
+        </div>
+
+    )
+}
+
+export default Programs
